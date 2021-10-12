e70caac289c0a9034cac6c4e28d893404f741f04
anibalcc97 <ac985015@ohio.edu>
2021-10-12 13:01:42 -0400

    Initial Commit

---
+++ src/main/java/edu/ohiou/mfgresearch/implanner/assembly/AssemblyFeature.java
@@ -0,0 +1,12 @@
+package edu.ohiou.mfgresearch.implanner.assembly;
+
+import edu.ohiou.mfgresearch.implanner.features.MfgFeature;
+
+public class AssemblyFeature extends MfgFeature {
+	
+	public static void main (String[] args) {
+		AssemblyFeature af = new AssemblyFeature ();
+		af.display("Display assy feature");
+	}
+
+}
